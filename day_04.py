6504859242da51b5a42527e466e19b4ac7073dbd
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-18 17:51:03 +0530

    Solution of 10th question from Hackerrank

--- day_04.py
+++ day_04.py
@@ -17,3 +17,27 @@ set_score = sorted(set(arr))
 print(set_score[-2])
 
 # =============================================================================================================
+
+# Question 10:- Nested List
+
+if __name__ == '__main__':
+    student = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        student.append([name,score])
+        
+n_sorte = sorted(student, key=lambda x: (x[1], x[0]))
+# print(n_sorte)
+
+smallest = (n_sorte[0])
+
+for i in n_sorte:
+    if i[1] > smallest[1]:
+        smallest = i
+        break
+        
+for n in n_sorte:
+    if n[1] == smallest[1]:
+        print(n[0])
+
