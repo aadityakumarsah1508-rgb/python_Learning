b44422f23e0239b5331800f0f4daf9748d5ba47a
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-20 22:04:50 +0530

    Solution of 12th question from Hackerrank

--- day_06.py
+++ day_06.py
@@ -1,4 +1,24 @@
 # Question  12: - Lists
 
+N = int(input())
+lis = []
+for _ in range(N):
+    query, *values = input().split()
+    value = list(map(int,values))
+        
+    if query == 'insert':
+        lis.insert(value[0],value[1])
+    if query == 'print':
+        print(lis)
+    if query == 'remove':
+        lis.remove(value[0])
+    if query == 'append':
+        lis.append(value[0])
+    if query == 'sort':
+        lis.sort()
+    if query == 'pop':
+        lis.pop()
+    if query == 'reverse':
+        lis.reverse()
 
 
