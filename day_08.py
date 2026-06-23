826421e64a8556ffebf9300c46e540cbfe5b74c2
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-23 21:21:03 +0530

    Solution of 18th question from Hackerrank

--- day_08.py
+++ day_08.py
@@ -26,3 +26,25 @@ if __name__ == '__main__':
     i, c = input().split()
     s_new = mutate_string(s, int(i), c)
     print(s_new)
+
+# ==========================================================================================
+
+# Question 18:- Strings
+
+def count_substring(string, sub_string):
+    count = 0
+    # Loop through the string up to the point where the substring can still fit
+    for i in range(len(string) - len(sub_string) + 1):
+        # Check if the slice matches the substring
+        if string[i : i + len(sub_string)] == sub_string:
+            count += 1
+            
+    return count
+        
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
