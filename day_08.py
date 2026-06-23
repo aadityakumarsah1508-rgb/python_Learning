dcf3c10ac5ac2d4dcb1d6a672e35dc57f34a2726
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-23 21:10:39 +0530

    Solution of 17th question from Hackerrank

--- day_08.py
+++ day_08.py
@@ -12,3 +12,17 @@ if __name__ == '__main__':
     print_full_name(first_name, last_name)
 
 # ===============================================================================================
+
+# Question 17:- Mutation
+
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position] = character
+    string = ''.join(l)
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
