567a011f206d2132c8203f16d053cdd72621ea1e
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-24 12:43:17 +0530

    Trying the methods

---
+++ day_09.py
@@ -0,0 +1,10 @@
+# Question 19:- String Validator
+
+# trying the methods
+s = 'Aadi1508'
+# print(s.isalnum())
+# print(s.isalpha())
+# print(s.isdigit())
+# print(s.islower())
+# print(s.isupper())
+
