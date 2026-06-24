337055a81d79938fe01d43090e03b027fb8e1c37
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-24 12:44:13 +0530

    Approaching the question

--- day_09.py
+++ day_09.py
@@ -8,3 +8,16 @@ s = 'Aadi1508'
 # print(s.islower())
 # print(s.isupper())
 
+# Approaching the question
+
+# l = []
+# for i in s:
+#     # l.append(i.isupper())
+#     print('True' ) if any(i.isupper()) else print('False')
+
+# # print(l)
+# # print(any(l))
+
+# final verdict
+print(any(l.isalnum() for l in s))
+
