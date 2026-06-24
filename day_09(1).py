4c788f00033dc2261d8cf052c49dca6c8bc3ca4f
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-24 21:09:46 +0530

    Solution of 20th question from Hackerrank

--- day_09(1).py
+++ day_09(1).py
@@ -32,19 +32,34 @@ for i in range(len(substr)):
     # print(map(f't{i}',substr.split()))
     pass
 
-u = []
-for i in substr:
-    for j in range(len(i)):
-        # 'U{j}' = i[j] + i[j+1]
-        # print(i[j])
-#         print(u)
+# u = []
+# for i in substr:
+#     for j in range(len(i)):
+#         # 'U{j}' = i[j] + i[j+1]
+#         # print(i[j])
+# #         print(u)
 
-        if i[j - 1] != i[j]:
-            u.append(i[j])
+#         # if i[j - 1] != i[j]:
+#         #     u.append(i[j])
 
-        # if i[j] not in u:
-        #     substr[j] = u.append(i[j])
-print(u)
+#         # if i[j] not in u:
+#         #     substr[j] = u.append(i[j])
+# print(u)
 
 for i in substr:
      print(*(set(i)), sep="")
+
+
+# =======================================================================
+
+# final answer
+
+string = 'AABCAAADA'
+
+n = len(string)
+q = int(n/k)
+    
+substr = [string[i:i+k] for i in range(0, n, k)]
+    
+for i in substr:
+    print(*(set(i)), sep="")
