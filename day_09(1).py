fc3b4e55ba31aa0648184abd7055b316d349a16b
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-24 19:14:32 +0530

    Trying to get Answer

--- day_09(1).py
+++ day_09(1).py
@@ -7,18 +7,44 @@ k = 3
 n = len(s)
 q = n/k
 
-# Trying to Finding logics 
-substring = []
-p = s[0:3]
-q = s[3:6]
-r = s[6:9]
-print(p, q, r)
+# # Trying to Finding logics 
+# substring = []
+# p = s[0:3]
+# q = s[3:6]
+# r = s[6:9]
+# print(p, q, r)
 
-# Inistating the Substring 
-for i in range(len(s)):
-    if i%k == 0:
-        a = s[i:(i+k)]
-        substring.append(a)
-        print(i)
-print(substring)
+# # Inistating the Substring 
+# for i in range(len(s)):
+#     if i%k == 0:
+#         a = s[i:(i+k)]
+#         substring.append(a)
+#         print(i)
+# print(substring)
 
+# after getting a help with AI
+substr = [s[i:i+k] for i in range(0, len(s), k)]
+# [s[i:i+k] for i in range(0, len(s), k)]
+print(substr)
+#----------------------------------------------------------------
+
+for i in range(len(substr)):
+    # print(map(f't{i}',substr.split()))
+    pass
+
+u = []
+for i in substr:
+    for j in range(len(i)):
+        # 'U{j}' = i[j] + i[j+1]
+        # print(i[j])
+#         print(u)
+
+        if i[j - 1] != i[j]:
+            u.append(i[j])
+
+        # if i[j] not in u:
+        #     substr[j] = u.append(i[j])
+print(u)
+
+for i in substr:
+     print(*(set(i)), sep="")
