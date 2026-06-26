8f79f3324e35f1d164c451a8f713c6c128c2d98a
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-26 22:43:39 +0530

    Understanding the Question

---
+++ day_11.py
@@ -0,0 +1,13 @@
+# Question 22:- String > Text wrap
+
+# Getting familiar with text wrap function....
+import textwrap
+
+line = "Hello Everyone, Here I am showing you all how I approach questions and finally solves them."
+# print(textwrap.wrap(line, 5))
+# print(textwrap.wrap(line, 8))
+# print(textwrap.wrap(line, 12))
+
+# print(textwrap.fill(line, 5))
+# print(textwrap.fill(line, 8))
+print(textwrap.fill(line, 12))
