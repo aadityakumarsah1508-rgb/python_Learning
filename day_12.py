94166371492119a01cf106d5da11bc6f3fdbad08
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-27 22:30:25 +0530

    Solution of 24th Question from Hackerrank

--- day_12.py
+++ day_12.py
@@ -75,5 +75,9 @@ for i in range(number):
 
 # Solution goes here
 width = len(bin(number)-2)
-for i in range(1, number+1):
-        print(str(i).rjust(width) + oct(i)[2:].rjust(width) + hex(i)[2:].rjust(width) + bin(i)[2:].rjust(width))
+# for i in range(1, number+1):
+#         print(str(i).rjust(width) + oct(i)[2:].rjust(width) + hex(i)[2:].rjust(width) + bin(i)[2:].rjust(width)) # It also gives error  as things were not aligned properly
+
+for i in range(1, number + 1):
+        print(f"{str(i).rjust(width)} {oct(i)[2:].rjust(width)} {hex(i)[2:].upper().rjust(width)} {bin(i)[2:].rjust(width)}")
+
