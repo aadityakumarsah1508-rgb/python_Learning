3db7bdcd117de52f65ff968018c9ff06e3aeaed5
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-29 23:23:51 +0530

    Trying to solve Question 25

--- day_13.py
+++ day_13.py
@@ -3,5 +3,21 @@
 # Approaching the question
 size = 3
 char_no = 96 + int(3)
+# print(chr(char_no))
 
-print(chr(char_no))
+# Trying to print things in for loop
+
+# for i in range(97, char_no + 1):
+#     print(chr(i))
+
+# Trying to solve problem with hard code.
+
+start = 97 
+end = 96 + 3
+direction = -1
+# print(end)
+while end != 96:
+    print(end)
+    end -= direction
+    if end == 97:
+        end += end
