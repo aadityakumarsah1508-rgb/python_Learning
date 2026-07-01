78e3a0663cb8f4d4b909cedf1f309666642c4474
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-01 17:47:33 +0530

    Approaching the question

---
+++ day_16.py
@@ -0,0 +1,19 @@
+# Question 28:- Itertools > itertools.product()
+
+from itertools import product
+
+# Approaching question
+
+# Taking input
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+# Verifying inputs
+print(a)
+print(b)
+
+# Printing Cartesian Product
+# print(product(a, b)) # it doesn't output anything
+pro = list(product(a,b))
+print(pro)
+
