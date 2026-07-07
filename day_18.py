0e87c9c7225221787a69f98b92bd6e6283ffaf2d
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-07 16:23:49 +0530

    Understanding the question

---
+++ day_18.py
@@ -0,0 +1,24 @@
+# Question 30:- Stings > The minions game
+
+s = input()
+
+kevin_score = 0
+sturt_score = 0
+
+vowelS = set("AEIOU")
+
+# len = len(s)
+# print(len)
+
+# doesn't working 
+# for i in range(len):
+#     # print(s[i]+"For loop with i")
+#     for j in range(len-i):
+#         # print(s[j]+"J loop")
+#         if s[i] in vowelS:
+#             print(s[j:])
+#     # if s[i] not in vowelS:
+#     #     print(s[i:])
+# 🤯🤯🤯😤😤😤😤
+
+
