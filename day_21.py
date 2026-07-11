638373b45bfced0ea32b52703e3bf9c702a2f8bc
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-11 19:29:33 +0530

    Approaching the question

--- day_21.py
+++ day_21.py
@@ -3,7 +3,7 @@
 from collections import defaultDict
 
 # Taking input from user
-n, m = list(map(str, input().split()))
+n, m = list(map(int, input().split()))
 
 group1 = []
 group2 = []
@@ -13,3 +13,15 @@ for i in range(n):
 
 for i in range(m):
     group2.append(input())
+
+
+
+# Appraoching the Question
+for i in group2:
+    if i in group1:
+        # print(i) -- it print the character itself we have to get its index
+        # print(group1.index(i)) -- it also doesn't work as it return only the first occurence
+        # we have to use enumerate
+        print([i for i, x in enumerate(group1) if i in group1 ])
+    else:
+        print(-1)
