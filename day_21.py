d82230280783e03c289f222d1201081ba42739f1
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-11 19:51:07 +0530

    Solution of 33rd question from Hackerrank

--- day_21.py
+++ day_21.py
@@ -1,6 +1,6 @@
 # Question 33:- Collections > DefaultDict Tutorial
 
-from collections import defaultDict
+from collections import defaultdict
 
 # Taking input from user
 n, m = list(map(int, input().split()))
@@ -14,7 +14,7 @@ for i in range(n):
 for i in range(m):
     group2.append(input())
 
-
+d = defaultdict()
 
 # Appraoching the Question
 for i in group2:
@@ -22,6 +22,9 @@ for i in group2:
         # print(i) -- it print the character itself we have to get its index
         # print(group1.index(i)) -- it also doesn't work as it return only the first occurence
         # we have to use enumerate
-        print([i for i, x in enumerate(group1) if i in group1 ])
+        # print([i for i, x in enumerate(group1) if i in group1 ])
+        d[i] = [a +1 for a, x in enumerate(group1) if x == i]
+        # print(d) 
+        print(" ".join(map(str, d[i])))
     else:
         print(-1)
