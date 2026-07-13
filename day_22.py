a82d74be89b4d5375001cac993a61f5d1bfb1cb8
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-13 11:16:46 +0530

    Solution of 34th question from Hackerrank

--- day_22.py
+++ day_22.py
@@ -8,7 +8,26 @@ m, d, y = map(int, input().split())
 # print("Printing the Date: ", d, m, y)
 
 # Attempting the question
+calendar.setfirstweekday(calendar.SUNDAY)
+print(calendar.firstweekday())
+
 day = calendar.weekday(year=y, month=m, day=d)
 
 print(day)
 
+# Remember Python treat 0 as Monday, 1 Tuesday and so on.
+ 
+if day == 0:
+    print("MONDAY")
+elif day == 1:
+    print("TUESDAY")
+elif day == 2:
+    print("WEDNESSDAY")
+elif day == 3:
+    print("THRUSDAY")
+elif day == 4:
+    print("FRIDAY")
+elif day == 5:
+    print("SATURDAY")
+elif day == 6:
+    print("SUNDAY")
