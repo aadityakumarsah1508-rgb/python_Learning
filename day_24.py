88eb56e6d19fccca39ef5691917f2f4d21664e6c
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-16 17:03:13 +0530

    Solution of 36th question from Hackerrank

--- day_24.py
+++ day_24.py
@@ -3,18 +3,36 @@
 from collections import namedtuple
 
 # learning namedtuple
-marks = namedtuple('marks', 'a, b')
-print(marks)
+# marks = namedtuple('marks', 'a, b')
+# print(marks)
 
 # x = int(input())
 # y = int(input())
 
-x, y = (input().split())
-student1 = marks(a=x, b=y)
-print(student1)
-print(student1.b)
+# x, y = (input().split())
+# student1 = marks(a=x, b=y)
+# print(student1)
+# print(student1.b)
 
 
+# N = int(input())
+# Student = namedtuple('Student', input().split())
+# # print(Student)
+# for i in range(N):
+    
+#      = Student(input().split())
+# # marks = [int(Student(*input().split()).MARKS) for _ in range(N)]
+# # print(f"{sum(marks) / N:.2f}")
+# print(Student.ID)    
 
+from collections import namedtuple
+
+N = int(input())
+Student = namedtuple('Student', input().split())
+marks = [int(Student(*input().split()).MARKS) for _ in range(N)]
+print(f"{sum(marks) / N:.2f}")  
+
+
+# need to learn about this
 
     
