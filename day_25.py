04345670122ea670437e06368277bbfef6d21201
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-18 22:05:20 +0530

    Learning the concept of datetime in py

---
+++ day_25.py
@@ -0,0 +1,17 @@
+# Question 37: Date and Time > Time Delta
+
+# Learing python Date from W3school
+# datetime module is used to work with dates as date objects.
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+# methods in datetime module
+print(x.year) # it returns the year
+print(x.strftime("%A")) # it return the name of the weekday
+
+# there are diff diff reference for fomat the code:
+
+
