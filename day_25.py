fce967971e40959bb22046a01b8825a11d576346
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-18 22:10:58 +0530

    Solution of 37th question from Hackerrank

--- day_25.py
+++ day_25.py
@@ -14,4 +14,15 @@ print(x.strftime("%A")) # it return the name of the weekday
 
 # there are diff diff reference for fomat the code:
 
+# Solution
+def time_delta(t1, t2):
 
+    fmt = "%a %d %b %Y %H:%M:%S %z"
+    
+    dt1 = datetime.strptime(t1, fmt)
+    dt2 = datetime.strptime(t2, fmt)
+    
+    seconds = str(abs(int((dt1 - dt2).total_seconds())))
+    return seconds
+
+# main function goes here.....
