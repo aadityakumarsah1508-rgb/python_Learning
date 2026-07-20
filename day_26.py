9677af606b98cd53a39a67845d20657159014548
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-21 00:04:37 +0530

    update: approaching solution

--- day_26.py
+++ day_26.py
@@ -6,3 +6,21 @@
 # Now we know the value of BC and MC, also tr(MCB) is iso. so 
 # <m + <C + <B = 180 
 # <B == <C so, 2<B + <M = 180 
+# this approach not worked
+
+import math
+
+ab = float(input())
+bc = float(input())
+
+#learning Python
+# angle = math.radians(ab//bc)
+# # angle = math.tan(angle_rad)
+# does not workk 
+
+# Solution from AI
+angle_rad = math.atan2(ab, bc)
+angle_deg = math.degrees(angle_rad)
+angle = int(angle_deg)
+# print(round(angle,"\u00b0"))
+print(f"{angle}\u00b0")
