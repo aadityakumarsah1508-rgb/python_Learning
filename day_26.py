2103a36aa45e6df18a64f1819c71c630c5e80399
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-21 00:02:21 +0530

    Approaching the 38th questioon

---
+++ day_26.py
@@ -0,0 +1,8 @@
+# Question 38:- Math > Find angle MBC
+
+# approach:- 
+# by pythagores theorem, sq(AC) = sq(AB) + sq(AC) 
+# MC = AC/2; 
+# Now we know the value of BC and MC, also tr(MCB) is iso. so 
+# <m + <C + <B = 180 
+# <B == <C so, 2<B + <M = 180 
